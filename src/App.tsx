9b0d19cbfb1c1c92883f016350976d85034d1b27
Roman <bolotovra@alfastrah.ru>
2022-09-07 16:22:00 +0300

    Добавлена обработка недоступных элементов внутри составного компонента

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,22 @@ const Checkbox = ({children}) => {
     console.log(checked);
 
     const allChildren = React.Children.map(children, child => {
+        console.log(child);
+
+        // можно вывести ошибку без рендеринга или вернуть null
+        // при return null элементы будут просто игнорироваться (без ошибки в консоли)
+        // if (typeof child.type === "string") {
+        //     // throw new Error(`<${child.type}/> DOM element is not allowed inside <Checkbox /> component`);
+        //     return null;
+        // }
+
+        // можно по-другому: проверка на соответствие только тем элементам, которые
+        // там должны быть (белый список поддерживаемых элементов)
+
+        if (child.type !== Label && child.type !== CheckboxInput) {
+            throw new Error(`No custom element supported`);
+        }
+
         const clone = React.cloneElement(child, {
             checked,
             setChecked
@@ -33,8 +49,8 @@ const Label = ({setChecked, children}) => {
 function App() {
     return (
         <Checkbox>
+            <Label>Chekbox label</Label>
             <CheckboxInput />
-            <Label>Chek box label</Label>
         </Checkbox>
     );
 }
